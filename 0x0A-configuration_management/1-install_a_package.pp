e14e9eb8d4735b9120a4120a6d7f7413e06a95de
siphoPrince <mabirimisesipho@gmail.com>
2023-11-24 03:51:41 -0800

    trying different ways

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,15 @@
 # Install Flask using the package resource
 
+# File: 1-install_a_package.pp
+
 exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin'],
-  creates => '/usr/lib/python3/dist-packages/Flask-2.1.0.dist-info',
+}
+
+exec { 'print_flask_version':
+  command => 'flask --version',
+  path    => ['/usr/bin'],
+  logoutput => true,
+  require => Exec['install_flask'],
 }
