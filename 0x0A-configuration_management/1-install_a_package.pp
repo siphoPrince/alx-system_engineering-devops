559b8081039fc385087aeeed8c72ef4433767512
siphoPrince <mabirimisesipho@gmail.com>
2023-11-24 03:54:25 -0800

    adding package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,10 +6,3 @@ exec { 'install_flask':
   command => '/usr/bin/pip3 install Flask==2.1.0',
   path    => ['/usr/bin'],
 }
-
-exec { 'print_flask_version':
-  command => 'flask --version',
-  path    => ['/usr/bin'],
-  logoutput => true,
-  require => Exec['install_flask'],
-}
