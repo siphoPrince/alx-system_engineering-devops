f26ace44ab370002c4f54b5c836e04b286b42ca1
siphoPrince <mabirimisesipho@gmail.com>
2023-11-24 04:06:29 -0800

    instaling using package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,7 +2,7 @@
 
 # File: 1-install_a_package.pp
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  path    => ['/usr/bin'],
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
