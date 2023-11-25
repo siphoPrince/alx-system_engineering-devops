897e3b5006e6e17767eed15bf3067889ba7029aa
siphoPrince <mabirimisesipho@gmail.com>
2023-11-25 01:50:47 -0800

    including ipo

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,16 +2,15 @@
 include stdlib
 
 file_line { 'Turn off passwd auth':
-  ensure  => present,
-  path    => '/home/your_username/.ssh/config',
-  line    => 'PasswordAuthentication no',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
   replace => true,
 }
 
-file_line { 'Declare identity file':
-  ensure  => present,
-  path    => '/home/your_username/.ssh/config',
-  line    => 'IdentityFile ~/.ssh/school',
+file_line { 'Delare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '     IdentityFile ~/.ssh/school',
   replace => true,
 }
-
