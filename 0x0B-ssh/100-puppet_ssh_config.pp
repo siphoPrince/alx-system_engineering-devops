3ae4a2e009daf452d0eceebab21b1049827bed1c
siphoPrince <mabirimisesipho@gmail.com>
2023-11-25 01:43:12 -0800

    using puppet to config without pw

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+#using pup to acces withou password
+
+file_line { 'Turn off passwd auth':
+  ensure  => present,
+  path    => '/home/your_username/.ssh/config',
+  line    => 'PasswordAuthentication no',
+  replace => true,
+}
+
+file_line { 'Declare identity file':
+  ensure  => present,
+  path    => '/home/your_username/.ssh/config',
+  line    => 'IdentityFile ~/.ssh/school',
+  replace => true,
+}
+
