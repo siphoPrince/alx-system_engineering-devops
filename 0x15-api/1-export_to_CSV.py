6b334b935923c3bca3cc578096e9572a555c3dc8
siphoPrince <mabirimisesipho@gmail.com>
2024-01-02 09:32:19 -0800

    scv in order

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,18 +3,16 @@
 For a given employee ID, returns information about his/her
 TODO list progress in CSV format.
 '''
-
+import csv
 import requests
 import sys
-import csv
-
 
 def get_employee_data(employee_id):
     """
     Get employee data and TODO list data from the REST API.
     """
     base_url = "https://jsonplaceholder.typicode.com"
-    
+
     user_response = requests.get(f"{base_url}/users/{employee_id}")
     user_data = user_response.json()
     employee_name = user_data.get('name')
@@ -24,6 +22,18 @@ def get_employee_data(employee_id):
 
     return employee_name, todo_data
 
+def display_todo_progress(employee_name, todo_data):
+    """
+    Display employee's TODO list progress.
+    """
+    total_tasks = len(todo_data)
+    completed_tasks = sum(task['completed'] for task in todo_data)
+
+    print(f"Employee {employee_name} is done with tasks({completed_tasks:02}/{total_tasks:02}):")
+
+    for task in todo_data:
+        if task['completed']:
+            print(f"\t{task['title']}")
 
 def export_to_csv(employee_id, employee_name, todo_data):
     """
@@ -38,7 +48,7 @@ def export_to_csv(employee_id, employee_name, todo_data):
         for task in todo_data:
             csv_writer.writerow([employee_id, employee_name, str(task['completed']), task['title']])
 
-
+    print(f"{csv_filename} created successfully")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -49,6 +59,8 @@ if __name__ == "__main__":
 
     try:
         employee_name, todo_data = get_employee_data(employee_id)
+        display_todo_progress(employee_name, todo_data)
         export_to_csv(employee_id, employee_name, todo_data)
+        print(f"sylvain@ubuntu$ cat {employee_id}.csv")
     except requests.RequestException as e:
         print(f"Error: {e}")
