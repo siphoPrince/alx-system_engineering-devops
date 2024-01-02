78d7b26cc9fa4d559a99457b48a24f49ccb873fd
siphoPrince <mabirimisesipho@gmail.com>
2024-01-02 09:45:44 -0800

    csv output

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -22,19 +22,6 @@ def get_employee_data(employee_id):
 
     return employee_name, todo_data
 
-def display_todo_progress(employee_name, todo_data):
-    """
-    Display employee's TODO list progress.
-    """
-    total_tasks = len(todo_data)
-    completed_tasks = sum(task['completed'] for task in todo_data)
-
-    print(f"Employee {employee_name} is done with tasks({completed_tasks:02}/{total_tasks:02}):")
-
-    for task in todo_data:
-        if task['completed']:
-            print(f"\t{task['title']}")
-
 def export_to_csv(employee_id, employee_name, todo_data):
     """
     Export TODO list data to CSV file.
@@ -52,15 +39,15 @@ def export_to_csv(employee_id, employee_name, todo_data):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
+        print("Usage: python3 1-export_to_CSV.py <employee_id>")
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
 
     try:
         employee_name, todo_data = get_employee_data(employee_id)
-        display_todo_progress(employee_name, todo_data)
         export_to_csv(employee_id, employee_name, todo_data)
-        print(f"sylvain@ubuntu$ cat {employee_id}.csv")
+        print(f"{employee_id}.csv: OK")
     except requests.RequestException as e:
         print(f"Error: {e}")
+
