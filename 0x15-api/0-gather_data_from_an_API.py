ebd5db11530201915c4165d0a3b1fcc4089b4aa7
siphoPrince <mabirimisesipho@gmail.com>
2024-01-03 00:10:56 -0800

    data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,51 +1,39 @@
 #!/usr/bin/python3
-'''
-printing a todo
-'''
+"""
+A script that gets the TODO list of a designated employee ID.
+"""
 
 import requests
-import sys
-
-
-def get_employee_data(employee_id):
-    """
-    Get employee data and TODO list data from the REST API.
-    """
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    user_response = requests.get(f"{base_url}/users/{employee_id}")
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
-
-    todo_response = requests.get(f"{base_url}/todos?userId={employee_id}")
-    todo_data = todo_response.json()
-
-    return employee_name, todo_data
-
-
-def display_todo_progress(employee_name, todo_data):
-    """
-    Display employee's TODO list progress.
-    """
-    total_tasks = len(todo_data)
-    completed_tasks = sum(task['completed'] for task in todo_data)
-
-    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-
-    for task in todo_data:
-        if task['completed']:
-            print(f"\t{task['title']}")
+from sys import argv
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
+    if len(argv) != 2 or not argv[1].isdigit():
+        print("Usage: {} employee_number".format(argv[0]))
+        exit(1)
 
-    employee_id = int(sys.argv[1])
+    employee_number = int(argv[1])
+
+    baselink = "https://jsonplaceholder.typicode.com"
+    employee_url = "{}/users/{}".format(baselink, employee_number)
+    tasks_url = "{}/todos?userId={}".format(baselink, employee_number)
 
     try:
-        employee_name, todo_data = get_employee_data(employee_id)
-        display_todo_progress(employee_name, todo_data)
-    except requests.RequestException as e:
-        print(f"Error: {e}")
+        employee_response = requests.get(employee_url)
+        tasks_response = requests.get(tasks_url)
+        employee_data = employee_response.json()
+        tasks_data = tasks_response.json()
+
+        employee_fullname = employee_data.get("name")
+        total_tasks = len(tasks_data)
+        completed_tasks = [task for task in tasks_data if task.get("completed")]
+
+        print("Employee {} has completed tasks({}/{}):".format(
+            employee_fullname, len(completed_tasks), total_tasks))
+
+        for task in completed_tasks:
+            print("\t {}".format(task.get("title")))
+
+    except requests.exceptions.RequestException as e:
+        print("Error: {}".format(e))
+        exit(1)
