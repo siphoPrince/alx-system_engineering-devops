e9d8163310de6940638d5ddc42ebbe43c08c1e7f
siphoPrince <mabirimisesipho@gmail.com>
2024-01-04 02:09:03 -0800

    list of directories

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,62 +1,31 @@
 #!/usr/bin/python3
-'''
-printing a todo
-'''
-import json
-import requests
-import sys
-
-
-def get_employee_data(employee_id):
-    """
-    Get employee data and TODO list data from the REST API.
-    """
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    user_response = requests.get(f"{base_url}/users/{employee_id}")
-    user_data = user_response.json()
-    employee_name = user_data.get('name')
-
-    todo_response = requests.get(f"{base_url}/todos?userId={employee_id}")
-    todo_data = todo_response.json()
-
-    return employee_name, todo_data
-
-
-def display_todo_progress(employee_name, todo_data):
-    """
-    Display employee's TODO list progress.
-    """
-    total_tasks = len(todo_data)
-    completed_tasks = sum(task['completed'] for task in todo_data)
-
-    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-
-    for task in todo_data:
-        if task['completed']:
-            print(f"\t{task['title']}")
-
-
-def export_to_json(employee_id, todo_data):
-    """
-    Export TODO data to JSON file.
-    """
-    filename = f"{employee_id}.json"
-    with open(filename, 'w') as json_file:
-        json.dump({employee_id: todo_data}, json_file, indent=2)
-    print(f"Data exported to {filename}")
-
+"""
+returns information about his/her TODO list progress and exports it to JSON.
+"""
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
 
-    employee_id = int(sys.argv[1])
+    import json
+    import requests
+    import sys
 
-    try:
-        employee_name, todo_data = get_employee_data(employee_id)
-        display_todo_progress(employee_name, todo_data)
-        export_to_json(employee_id, todo_data)
-    except requests.RequestException as e:
-        print(f"Error: {e}")
+    user_id = sys.argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}/'.format(user_id)
+    todos_url = url + 'todos'
+    user = requests.get(url).json()
+    todos = requests.get(todos_url).json()
+
+    todo_list = []
+    for todo in todos:
+        new_dict = {}
+        new_dict['task'] = todo.get('title')
+        new_dict['completed'] = todo.get('completed')
+        new_dict['username'] = user.get('username')
+        todo_list.append(new_dict)
+
+    todo_dict = {user.get('id'): todo_list}
+
+    file_name = '{}.json'.format(user.get('id'))
+
+    with open(file_name, mode='w') as outfile:
+        json.dump(todo_dict, outfile)
--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """Exporting all employees to JSON format."""
 import json
 import requests
