4b77b9144bc06432c10d99c3f6b43c948e6568be
siphoPrince <mabirimisesipho@gmail.com>
2024-01-17 05:55:26 -0800

    file error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,11 +1,2 @@
 # Typo in Wordpress config file fix
-file { '/var/www/html/wp-includes/class-wp-locale_backup.php':
-  ensure => 'file',
-  source => '/var/www/html/wp-includes/class-wp-locale.php',
-}
-
-file { '/var/www/html/wp-includes/class-wp-locale.php':
-  ensure  => 'absent',
-  require => File['/var/www/html/wp-includes/class-wp-locale_backup.php'],
-}
-
+exec { '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php': }
