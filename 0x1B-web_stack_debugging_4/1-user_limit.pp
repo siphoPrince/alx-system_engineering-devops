313d75af4802d207ac825d43141d34c18b356cd5
siphoPrince <mabirimisesipho@gmail.com>
2024-01-24 22:35:31 -0800

    user limit space

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,4 @@
-#possible to login with the holberton user and open a file without any error message
-
+#login with the holberton user and open a file without any error message
 exec {'hard-limit':
     command  => 'sed -i "/holberton hard/s/5/10000" /etc/security/limits.conf',
     provider => shell,
